0e8cd2290c4215d5dd97752465d9bdb03885b6df
Kevin O'Neill <koneill1994@gmail.com>
2017-04-03 18:35:13 -0400

    added list of all possible glyphs

--- sprite.py
+++ sprite.py
@@ -17,9 +17,15 @@ list of 12 bools describe whether squares are connected or not
 
 '''
 
+# size of the icon
+# keep this at 32 unless you want to rewrite a bunch of code
+icon=32
+
+# number down and across of glyphs to make
 h=32
 w=32
 
+# locations to place bridges
 bridges=[
   (9,2,True),(19,2,True),
   (2,9,False),(12,9,False),(22,9,False),
@@ -28,11 +34,13 @@ bridges=[
   (9,22,True),(19,22,True)
 ]
 
+# locations to place holes
 holes=[
 (10,10),(20,10),
 (10,20),(20,20)
 ]
 
+# bridges to check and see if a hole is necessary
 hole_corr=[
 [0,2,3,5],
 [1,3,4,6],
@@ -40,13 +48,21 @@ hole_corr=[
 [6,8,9,11]
 ]
 
-im_array = [[255 for col in range(w)] for row in range(h)]
+im_array = [[255 for col in range(icon*w)] for row in range(icon*h)]
 
 def RandomGlyphCode():
   output=[]
   for i in range(0,12):
     output.append(random.randrange(0,2))
   return output
+
+def GlyphFromNumber(n):
+  output=[]
+  b=bin(n)[2:]
+  c=[0]*(12-len(b))+list(b)
+  for e in c:
+    output.append(int(e))
+  return list(reversed(output))
 
 def draw_empty_block((x,y),array):
   s=8
@@ -112,15 +128,18 @@ def test_drawing(im_array):
     im_array=draw_hole(l,im_array)
   return im_array
 
-def draw_glyph(code,array):
+def add_tuple((a,b),f):
+  return (f[0]+a,f[1]+b)
+
+def draw_glyph((a,b),code,array):
   # draw empty blocks
   for i in range(3):
     for j in range(3):
-      array=draw_empty_block((2+10*i,2+10*j),array)
+      array=draw_empty_block((2+10*i+a,2+10*j+b),array)
   #draw bridges
   for i in range(len(code)):
     if(code[i]):
-      array=draw_bridge(bridges[i],array)
+      array=draw_bridge((bridges[i][0]+a,bridges[i][1]+b,bridges[i][2]),array)
   #draw holes
   for n in range(len(hole_corr)):
     t=True
@@ -128,23 +147,23 @@ def draw_glyph(code,array):
       if(not code[m]):
         t=False
     if(t):
-      array=draw_hole(holes[n],array)
+      array=draw_hole(add_tuple((a,b),holes[n]),array)
   return array
 
-k=RandomGlyphCode()
+for m in range(h):
+  for n in range(w):
+    k=RandomGlyphCode()
+    #k=GlyphFromNumber(m*w+n)
+    #print k
+    im_array=draw_glyph((n*icon,m*icon),k,im_array)
 
-print k
-im_array=draw_glyph(k,im_array)
 
-
-
-
-img = Image.new( 'RGB', (h,w), "white") # create a new white image
+img = Image.new( 'RGB', (icon*h,icon*w), "white") # create a new white image
 pixels = img.load() # create the pixel map
 
 
-for y in range(h):
-  for x in range(w):
+for y in range(icon*h):
+  for x in range(icon*w):
     c=im_array[y][x]
     pixels[x, y] = (c,c,c)
 
