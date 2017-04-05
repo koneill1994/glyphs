858bb7a546c860770216dc34be25be32baf354a7
Kevin O'Neill <koneill1994@gmail.com>
2017-04-05 14:59:20 -0400

    Added compound word solver

--- translate.py
+++ translate.py
@@ -1,4 +1,6 @@
 
+import math
+
 # list to allow numbers to be translated into their constituent english digits
 digits=[
   'zero',
@@ -10,8 +12,7 @@ digits=[
   'six',
   'seven',
   'eight',
-  'nine',
-  'ten'
+  'nine'
 ]
 
 # all 26 english letters are in the list
@@ -25,6 +26,27 @@ def is_int(k):
   except ValueError:
     return False
 
+def break_up_compound_word(word):
+  output=[]
+  for i in range(1,len(word)-1):
+    p=[]
+    t=True
+    try:
+      #print(word[:i])
+      p.append(words.index(word[:i].lower()+'\n'))
+      #print(word[i:])
+      p.append(words.index(word[i:].lower()+'\n'))
+    except ValueError:
+      #print '    valueerror'
+      t=False
+    #print
+    if(t): output.append(p)
+  print(output)
+  if(len(output)>0):
+    # return the pair split nearest to the center
+    return output[int(math.floor(len(output)/2.0))]
+  else: return [word]
+
 
 with open('scrambled_list.txt') as f:
     words = f.readlines()
@@ -32,17 +54,25 @@ with open('scrambled_list.txt') as f:
 # input text to have its common words translated into the scrambled_list.txt encoding
 
 while True:
+  print(break_up_compound_word(raw_input()))
+
+while True:
   m=raw_input('').split()
-  p=''
+  glyphs=[]
+  p=[]
   for n in m:
     try:
-      p+=str(words.index(n.lower()+'\n'))
+      p.append(words.index(n.lower()+'\n'))
+      glyphs.append(n)
     except ValueError:
       if(is_int(n)):
         for char in n:
-          p+=str(words.index(digits[int(char)]+'\n'))  +' '
+          p.append(words.index(digits[int(char)]+'\n'))
+          glyphs.append(digits[int(char)])
       else:
         for char in n:
-          p+=str(words.index(char.lower()+'\n'))+' '
-    if(p[-1]!=' '): p+=' '
+          p.append(words.index(char.lower()+'\n'))
+          glyphs.append(char)
+  print glyphs
   print p
+  
